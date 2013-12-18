e7ae9868e2c6cb7340cb0f81eee6ecb4d2cbf1c1
mihokondoh <mihokondoh@gmail.com>
2013-12-19 04:33:07 +0900

    データセット変更

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'nagareyama_h25_yosan_00';
+OpenSpending.identifier = 'nagareyama_2013_yosan_00';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
